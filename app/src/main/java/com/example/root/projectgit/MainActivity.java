d01b79f22024014e8dad1211bc0e7ac1dbb48673
Umberto Barbato <umberto.barbato@icloud.com>
2018-03-26 15:29:04 +0200

    Setting references

--- app/src/main/java/com/example/root/projectgit/MainActivity.java
+++ app/src/main/java/com/example/root/projectgit/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.root.projectgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    //View Layout
+    private Button vbtn_add;
+    private Button vbtn_sub;
+    private TextView vtxt_click;
 
 
     @Override
@@ -11,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //View references
+        vbtn_add = findViewById(R.id.btn_add);
+        vbtn_sub = findViewById(R.id.btn_sub);
+        vtxt_click = findViewById(R.id.txt_click);
+        
     }
 
 }
