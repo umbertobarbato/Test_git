d428100b44e45dbd8be8e2ec870b3ed94b1d3e6a
Umberto Barbato <umberto.barbato@icloud.com>
2018-03-26 15:38:51 +0200

    Added Events Listeners and subroutine to view int variable;

--- app/src/main/java/com/example/root/projectgit/MainActivity.java
+++ app/src/main/java/com/example/root/projectgit/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.root.projectgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 
@@ -10,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private Button vbtn_add;
     private Button vbtn_sub;
     private TextView vtxt_click;
-
+    private int cont = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +22,30 @@ public class MainActivity extends AppCompatActivity {
         vbtn_add = findViewById(R.id.btn_add);
         vbtn_sub = findViewById(R.id.btn_sub);
         vtxt_click = findViewById(R.id.txt_click);
-        
+        //Set Events Listeners
+        vbtn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cont = cont + 1;
+                setTxttoCont();
+            }
+        });
+        vbtn_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cont = cont - 1;
+                setTxttoCont();
+            }
+        });
+
     }
+
+    /**
+     * Subroutine set text of label on cont;
+     */
+    private void setTxttoCont()        {
+        vtxt_click.setText(String.format(getString(R.string.labelclick_text), cont));
+    }
+
 
 }
